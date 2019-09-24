1a92122deb37360b9f747352483ff0207ff83b99
adel.ddr <dadrasadel@gmail.com>
2019-09-24 12:39:39 +0330

    edit pages

---
+++ app/src/main/java/com/kasra/kasrahamrah/ui/main/fragments/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package com.kasra.kasrahamrah.ui.main.fragments.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
